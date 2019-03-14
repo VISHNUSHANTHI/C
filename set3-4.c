c0ae0a6dbc29ee4913eba07998a506ea17bb6e1a
VISHNU.P <47105744+VISHNUSHANTHI@users.noreply.github.com>
2019-03-14 15:53:47 +0530

    Create set3-4.c

---
+++ set3-4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],temp,n,i,j;
+printf("Enetr the array size:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf("\n%d",a[i]);
+}
+getch();
+}
