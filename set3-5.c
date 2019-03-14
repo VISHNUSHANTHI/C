fa6276db1e9891f7507b887f95e70df818b11061
VISHNU.P <47105744+VISHNUSHANTHI@users.noreply.github.com>
2019-03-14 15:58:41 +0530

    Create set3-5.c

---
+++ set3-5.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],temp,n,i,j;
+prinyf("Enter the value:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+if(n%2==0)
+{
+n=n/2;
+printf("\n%d",a[n-1]);
+}
+else
+{
+n=n/2;
+printf("\n%d",a[n]);
+}
+getch();
+}
