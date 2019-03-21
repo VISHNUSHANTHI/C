4b3499be7d5814eabbbdeb47ec82cefe9aeca411
VISHNU.P <47105744+VISHNUSHANTHI@users.noreply.github.com>
2019-03-21 15:58:00 +0530

    Create set3-8.c

---
+++ set3-8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[100],i,n;
+printf("Enetr the array size :");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[n]);
+printf("%d \t %d\n",a[n],i);
+}
+}
