ff42574c573d81f605786bae1596b809ad9c1bca
VISHNU.P <47105744+VISHNUSHANTHI@users.noreply.github.com>
2019-03-25 16:20:34 +0530

    Update set5-8.c

--- set5-8.c
+++ set5-8.c
@@ -2,20 +2,15 @@
 #include<conio.h>
 int main()
 {
-    int n,a[10],i,sum=0;
-    int avg;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-        sum=sum+a[i];
-    }
-   
-    avg=sum/n;
-    
-    printf("%d",avg);
-   
-    
-
-    return 0;
+int n,a[10],i,sum=0;
+int avg;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+sum=sum+a[i];
+}
+avg=sum/n;
+printf("%d",avg);
+return 0;
 }
