27151ce4b8a0595e88a582fcc377f1d0dcc22fee
VISHNU.P <47105744+VISHNUSHANTHI@users.noreply.github.com>
2019-03-25 16:30:49 +0530

    Create set6-1.c

---
+++ set6-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+int num,rem,rev=0;
+printf("enter the number:");
+scanf("%d",&num);
+while(num!=0)
+{
+rem=num%10;
+rev=(rev*10)+rem;
+num=num/10;
+}
+num=rev;
+while(num!=0)
+{
+rem=num%10;
+printf("%d ",rem);
+num=num/10;
+}
+return 0;
+}
