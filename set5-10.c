f5d81eebe2890c144dd6d137079f2b9c3548ad0f
VISHNU.P <47105744+VISHNUSHANTHI@users.noreply.github.com>
2019-03-25 16:26:51 +0530

    Update set5-10.c

--- set5-10.c
+++ set5-10.c
@@ -2,20 +2,20 @@
 #include<conio.h>
 int main()
 {
-	int n,i,x=1,pow=2;
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-	    x=x*pow;
-	    if(x==n)
-	    {
-	        printf("%d is a power of 2",n);
-	        break;
-	    }
-	}
-        if(x!=n)
-	    {
-	        printf("%d is not a power of 2",n);
-	    }
-	return 0;
+int n,i,x=1,pow=2;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{	    
+x=x*pow;
+if(x==n)
+{
+printf("%d is a power of 2",n);
+break;
+}
+}
+if(x!=n)
+{
+printf("%d is not a power of 2",n);
+}
+return 0;
 }
