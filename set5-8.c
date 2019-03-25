c420165b4262a6f1bf632a58bd8e6679990b0a90
VISHNU.P <47105744+VISHNUSHANTHI@users.noreply.github.com>
2019-03-25 16:18:55 +0530

    Create set5-8.c

---
+++ set5-8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,a[10],i,sum=0;
+    int avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        sum=sum+a[i];
+    }
+   
+    avg=sum/n;
+    
+    printf("%d",avg);
+   
+    
+
+    return 0;
+}
