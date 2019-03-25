76278332c5ec59ac7d03c1762fddf41a8a5a2ef3
VISHNU.P <47105744+VISHNUSHANTHI@users.noreply.github.com>
2019-03-25 16:22:39 +0530

    Create set5-9.c

---
+++ set5-9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main
+{
+int n1,n2,sum;
+printf("\n enter the first number:");
+scanf("%d",&n1);
+printf("\n enter the second number:");
+scanf("%d",&n2);
+sum=n1+n2;
+printf("%d",sum);
+getch();
+}
